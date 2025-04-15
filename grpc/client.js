4643a9eef1bd49f7ac4e5b253b3047bac4bac625
Pramit Amatya <130182347+pumpking786@users.noreply.github.com>
2025-04-15 17:22:37 +0545

    Update client.js

--- grpc/client.js
+++ grpc/client.js
@@ -1,23 +1,23 @@
-// grpc/client.js
-const grpc = require('@grpc/grpc-js');
-const protoLoader = require('@grpc/proto-loader');
+// // grpc/client.js
+// const grpc = require('@grpc/grpc-js');
+// const protoLoader = require('@grpc/proto-loader');
 
-// Load proto files
-const authProtoPath = './proto/auth.proto';
-const blogProtoPath = './proto/blog.proto';
+// // Load proto files
+// const authProtoPath = './proto/auth.proto';
+// const blogProtoPath = './proto/blog.proto';
 
-const authPackageDef = protoLoader.loadSync(authProtoPath);
-const blogPackageDef = protoLoader.loadSync(blogProtoPath);
+// const authPackageDef = protoLoader.loadSync(authProtoPath);
+// const blogPackageDef = protoLoader.loadSync(blogProtoPath);
 
-// Load gRPC Packages
-const authProto = grpc.loadPackageDefinition(authPackageDef);
-const blogProto = grpc.loadPackageDefinition(blogPackageDef);
+// // Load gRPC Packages
+// const authProto = grpc.loadPackageDefinition(authPackageDef);
+// const blogProto = grpc.loadPackageDefinition(blogPackageDef);
 
-// Create gRPC Clients
-const authStub = new authProto.AuthService('0.0.0.0:50051', grpc.credentials.createInsecure());
-const blogStub = new blogProto.BlogService('0.0.0.0:50052', grpc.credentials.createInsecure());
+// // Create gRPC Clients
+// const authStub = new authProto.AuthService('0.0.0.0:50051', grpc.credentials.createInsecure());
+// const blogStub = new blogProto.BlogService('0.0.0.0:50052', grpc.credentials.createInsecure());
 
-module.exports = {
-  authStub,
-  blogStub,
-};
+// module.exports = {
+//   authStub,
+//   blogStub,
+// };
