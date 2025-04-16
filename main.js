c2082bd35def44fd2fb5800c149507955b0fb196
Pramit Amatya <130182347+pumpking786@users.noreply.github.com>
2025-04-16 17:40:22 +0545

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 // grpc/client.js
 const grpc = require('@grpc/grpc-js');
 const protoLoader = require('@grpc/proto-loader');
+const express = require('express');
 
+const app=express();
+app.use(express.json())
 // Load proto files
 const authProtoPath = './proto/auth.proto';
 const blogProtoPath = './proto/blog.proto';
@@ -17,4 +20,34 @@ const blogProto = grpc.loadPackageDefinition(blogPackageDef);
 const authStub = new authProto.AuthService('0.0.0.0:50051', grpc.credentials.createInsecure());
 const blogStub = new blogProto.BlogService('0.0.0.0:50052', grpc.credentials.createInsecure());
 
+app.post('/register',(req,res)=>{
+    const {username,password}=req.body;
 
+    authStub.register({username,password},(err,response)=>{
+        if (err) {
+            console.error('gRPC register error:', err);
+            return res.status(400).json({ message: err.details });
+          }
+      
+          res.status(200).json({ success: response.success });
+        });
+})
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+  
+    authStub.login({ username, password }, (err, response) => {
+      if (err) {
+        console.error('gRPC login error:', err);
+        return res.status(400).json({ message: err.details });
+      }
+  
+      res.status(200).json({ token: response.token });
+    });
+  });
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`REST API for AuthService running at http://localhost:${PORT}`);
+});
